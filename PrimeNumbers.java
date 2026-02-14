7bd2bae693a4226e01735ee77304cbfbdbfd6682
Dinesh E <dineshd5058@gmail.com>
2026-02-14 17:20:07 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 
-class PrimeNumbersCheck
+class PrimeNumbersCheck 
 {
-	public static void main(String[] args)
-	{
-		
-	
-		Scanner scan=new Scanner(System.in);System.out.print("Enter integer number: ");
-		int n=scan.nextInt();
-		for(int i=2;i<n;i++)
-		{
-			boolean isPrime = true;
-			for(int j=2;j<i/2;j++)
-			{
-				if(i%j==0)
-				{
-              isPrime=false;
-               break;
-			   }
-   }
-      if(isPrime)
-     {
-				      System.out.println(i);
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter the integer number");
+        int n = scan.nextInt();
+
+        System.out.println("Prime numbers up to " + n);
+
+        for(int i = 2; i <= n; i++)
+        {
+            boolean isPrime = true;
+
+            for(int j = 2; j <= i / 2; j++)
+            {
+                if(i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime)
+            {
+                System.out.println(i);
+            }
         }
-       }
-         scan.close();
-     }
-       }
+
+        scan.close();
+    }
+}
